1c5c0feeff052b10cc6b50ee1563b2a0abfa7f8f
andrjak <79821843rt@gmail.com>
2023-11-14 18:40:10 +0300

    Add FolderListner.cs

---
+++ MessageQueues/MessageQueuesFileScanner/DataCaptureService/Program.cs
@@ -0,0 +1,27 @@
+﻿using DataCaptureService;
+
+#region Init functionality
+var listner = new FolderListener(@"D:\TestFolder");
+listner.Created += OnCreated;
+
+static void OnCreated(object sender, FileSystemEventArgs e)
+{
+    string value = $"Created: {e.FullPath}";
+    Console.WriteLine(value);
+}
+#endregion
+
+
+#region Work with CommandLineInterface
+Console.WriteLine("Use 'q' key to exit...");
+
+string input = string.Empty;
+
+for (;input.Trim().ToUpper() != "Q";)
+{
+    input = Console.ReadLine() ?? string.Empty;
+}
+
+Console.WriteLine("Press any key to continue");
+Console.ReadLine();
+#endregion
